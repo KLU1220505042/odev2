29d91b0996dbae24c9ceb4b514e6c707dffbdb8c
KLU1220505042 <113675181+KLU1220505042@users.noreply.github.com>
2023-03-15 21:10:39 +0300

    AlgoritmaAnalizi

---
+++ AlgoritmaAnalizi/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	
+	 // Algoritmayý neden analiz ederiz?
+
+	// Algoritma analizini, bir algoritmanýn performansýný anlamak ve deðerlendirmek için yaparýz. 
+	// Algoritmanýn doðru çalýþtýðý varsayýldýðý durumda, algoritmanýn çalýþma zamaný ve bellek kullanýmý gibi performans ölçütlerini inceleriz. 
+	// Bu analiz sonucunda, oluþturulmuþ olan algoritmanýn verimli olup olmadýðýný ve büyük veri kümeleri gibi yoðun uygulama senaryolarýnda nasýl davranacaðýný anlamamýza yardýmcý olur.
+
+    // Algoritma analizini nasýl yaparýz?
+
+	// " 1-)	Ýlk olarak, algoritmanýn doðru çalýþtýðýndan emin olunmalýdýr. Algoritmayý birkaç farklý girdi deðeri ile test ederek ve sonuçlarýn doðru olduðundan emin olmalýsýnýz.
+	// " 2-)	Algoritmanýn çalýþmasý sýrasýnda ne kadar iþlem yapacaðýný belirlemek için iþlem sayýsýný hesaplayýn. 
+	// Bu, döngüler, karþýlaþtýrmalar, atamalar ve diðer iþlemleri sayarak yapýlabilir.
+	// " 3-)	Algoritmanýn çalýþma zamanýný belirleyin. Bu, iþlem sayýsý ile algoritmanýn her bir iþleminin ne kadar sürede gerçekleþeceði arasýndaki iliþkiyi dikkate alarak yapýlabilir.
+	// " 4-)	Algoritmanýn ne kadar bellek kullanacaðýný belirleyin. Bu, algoritmanýn deðiþkenlerinin, dizilerinin ve diðer veri yapýlarýnýn boyutunu dikkate alarak yapýlabilir.
+	// " 5-)	Algoritmanýn performansýný diðer algoritmalarla karþýlaþtýrýn. 
+	// Bu, ayný iþi yapan diðer algoritmalarýn çalýþma zamanýný ve bellek kullanýmýný da belirleyerek yapýlabilir.
+	// " 6-) 	Algoritma analizi, algoritmalarýn verimliðini arttýrmak için önemlidir. 
+	// Ýyi bir algoritma analizi, algoritmanýn doðru çalýþtýðýndan ve verimli olduðundan emin olmak için yapýlmalýdýr.
+
+	
+	
+	
+	
+	return 0;
+}
